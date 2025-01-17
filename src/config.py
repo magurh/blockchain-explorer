9a8fea509a2c17c8b3a81aa03ecd56d7203bd591
magurh <magureanuhoria@gmail.com>
2025-01-17 22:51:16 +0000

    feat: query block data

---
+++ src/config.py
@@ -0,0 +1,21 @@
+import os
+from dataclasses import dataclass
+
+from dotenv import load_dotenv
+
+load_dotenv()
+
+
+@dataclass(frozen=True, kw_only=True)
+class Config:
+    rpc_url: str
+
+def load_env_var(var_name: str) -> str:
+    """Loads and validates environment variables."""
+    return os.getenv(var_name, default="")
+
+
+# Initialize configuration
+config = Config(
+    rpc_url=load_env_var("RPC_URL"),
+)
---
+++ src/utils.py
@@ -0,0 +1,11 @@
+import json
+
+def save_block(block_data, filename):
+    """
+    Saves block data to a JSON file.
+    :param block_data: The block data dictionary.
+    :param filename: The filename to save the JSON data.
+    """
+    with open(filename, 'w') as json_file:
+        son.dump(block_data, json_file, indent=4)
+        print(f"Block data saved to {filename}")
